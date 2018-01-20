d7c746cc50401a270badeebde62e0b2aaec5ed14
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-01-20 17:34:34 +0100

    Update jqueryComp.js

--- WebRoot/js/treeview/jqueryComp.js
+++ WebRoot/js/treeview/jqueryComp.js
@@ -19,18 +19,3 @@ function setSettings(settings) {
         }));
     }
 
-function createNode(parent) {
-	document.writeln("<script type='text/javascript' src='functions.js'></script>");
-	var spanText = "<span>" + this.text + "</span>";
-	var test = ("<li/>").attr("id", this.id || "");
-	test.html(spanText);
-	test.appendTo(parent);
-    var current = $.test;
-    if (this.classes) {
-        current.children("span").addClass(this.classes);
-    }
-    if (this.expanded) {
-        current.addClass("open");
-    }
-    newFunction();
-}
